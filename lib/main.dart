03417462a35f8c714d378b2684435916c8b428cd
Gabriel Davi <gdavi.gd@gmail.com>
2020-08-30 21:22:16 -0300

    Creating a stateful component'

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,53 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 main() => runApp(new PerguntaApp());
 
-@override
-class PerguntaApp extends StatelessWidget {
+class _PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual a sua cor favorita?',
+      'Qual é seu animal favorito?'
+    ];
+
     return new MaterialApp(
-      home: Text("Ola flutter"),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Perguntas"),
+      ),
+      body: Column(
+        children: [
+          Questao(perguntas[perguntaSelecionada]),
+          RaisedButton(
+            child: Text('Resposta 1'),
+            onPressed: _responder,
+          ),
+          RaisedButton(
+            child: Text('Resposta 2'),
+            onPressed: _responder,
+          ),
+          RaisedButton(
+            child: Text('Resposta 3'),
+            onPressed: _responder,
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
   }
 }
